4b26652d0eef058e99985ac3cb4209922a2dd1ca
Sibeykin <akkbronza4@gmail.com>
2025-02-24 01:05:40 +0300

    Add files via upload

---
+++ 16.2.pas
@@ -0,0 +1,36 @@
+﻿program R162;
+
+var
+  inputFile: Text;
+  wordList: array of string;
+  word: string;
+  count, i: Integer;
+  found: Boolean;
+begin
+  Assign(inputFile, '16.2.txt');
+  Reset(inputFile);
+  
+  count := 0;
+  while not Eof(inputFile) do
+  begin
+    ReadLn(inputFile, word);
+    found := False;
+    
+    for i := 0 to count - 1 do
+      if wordList[i] = word then
+      begin
+        found := True;
+        Break;
+      end;
+    
+    if not found then
+    begin
+      SetLength(wordList, count + 1);
+      wordList[count] := word;
+      count := count + 1;
+    end;
+  end;
+  Close(inputFile);
+  
+  Writeln('количество слов: ', count);
+end.
---
+++ 16.3.pas
@@ -0,0 +1,19 @@
+﻿program R163;
+var
+  numbers: array[1..10] of Integer;
+  i: Integer;
+begin
+  for i := 1 to 10 do
+    numbers[i] := i; 
+  
+  Write('список: ');
+  for i := 1 to 10 do
+    Write(numbers[i], ' ');
+  Writeln;
+  
+  Write('четные: ');
+  for i := 1 to 10 do
+    if numbers[i] mod 2 = 0 then
+      Write(numbers[i], ' ');
+  Writeln;
+end.
